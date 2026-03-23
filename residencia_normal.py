514950d09dc774641ad6ae129d184f2b528b5d14
Mileni Lelis Ramos Sérvulo <lelis.engineer@yahoo.com>
2026-03-23 10:50:17 -0300

    Fix water consumption calculation and output

--- residencia_normal.py
+++ residencia_normal.py
@@ -4,24 +4,17 @@
 # Se o consumo for menor ou igual a 50m3, então R$ 8,75 por m3
 # Se o consumo for acima dos 50m3, então R$ 9,64 por m3
 
-consumon=float(input("Digite o consumo de aguá da redisência em m3:"))
+consumon= float(input("Digite o consumo de aguá da sua redisência em m3:"))
 
 
-if consumon <= pow(10, 3) :
-    valor = consumon * 22,38
-    print("O consumo de aguá de sua residência irá custar   por m3")
-
-elif consumon <=  pow(20, 3) : 
-     valor = consumon * 3,50
-     print("O consumo de aguá de sua residência irá custar  por m3 ")
-
-elif consumon <=  pow(30, 3) : 
-     valor = consumon * 8,75
-     print("O consumo de aguá de sua residência irá custar por  m3 ")
-
-elif consumon <=  pow(40, 3) : 
-     
-     print("O consumo de aguá de sua residência irá custar  por m3 ")
-
+if consumon <= pow(10, 3):
+    valor = consumon * 22.38
+    
+elif consumon <=  pow(20, 3): 
+     valor = consumon * 3.50
+elif consumon <=  pow(50, 3): 
+     valor = consumon * 8.75
 else:
-     print("Seu consumo não se enquadrada em nenhuma das váriaveis possíveis")   
+     valor = consumon * 9.64
+
+print(f"O consumo de água de sua residência irá custar R$ {valor:.2f}")
