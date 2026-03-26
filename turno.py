2d97647ea0091c6ac177ab9a5e0b92b3fa257b80
Mileni Lelis Ramos Servulo <lelis.engineer@yahoo.com>
2026-03-26 10:22:47 -0300

    Update turno.py

--- turno.py
+++ turno.py
@@ -1,22 +1,23 @@
 #Pergunte o turno da pessoa e retorne uma saudação
 
+#Entrada
 print("Escolha uma opção de turno")
 print("Opções: M-Matutino, V-Vespertino, N-Noturno")
 t=input()
 
 
-
-if t == "M":
+#Processamento
+if t == "M":  #Aqui será a entrada do turno matutino
     tr=("Bom dia!")
 
-elif t == "V":
+elif t == "V":  #Aqui será a entrada do turno Vespertino
     tr=("Boa tarde!")
 
-elif t == "N":
+elif t == "N":  #Aqui será a entrada do turno Noturno
     tr=("Boa noite!")
 
-else:
+else:     #Caso a pessoa não escolha nenhuma das opções esse será seu retorno
     tr=("Valor inválido!")
-  
+#Saída  
 print(f"{tr}")
 
