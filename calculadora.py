d0b3d563335322a2d5077a56823a40aa90793ca5
Mileni Lelis Ramos Servulo <lelis.engineer@yahoo.com>
2026-03-26 09:59:47 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -4,26 +4,28 @@ import math
 
 n1 = float(input("Digite o primeiro número: "))
 n2 = float(input("Digite o segundo número: "))
-escolha = input("Operação: ").lower()
+print("Opções: +, -, *, /, **, raiz q, /2, impar")
+escolha = input("")
+
 
  
 
-if escolha == "adição":
+if escolha == "+":
     r = n1 + n2
-elif escolha == "subtração":
+elif escolha == "-":
     r = n1 - n2
-elif escolha == "multiplicação":
+elif escolha == "**":
     r = n1 * n2
-elif escolha == "divisão":
+elif escolha == "/":
     r = n1 // n2 
-elif escolha == "potência":
+elif escolha == "^":
     r = n1 ** n2
-elif escolha == "raiz quadrada":
+elif escolha == "raiz q":
     r = f"Raiz de {n1} é {math.sqrt(n1):.2f} e de {n2} é {math.sqrt(n2):.2f}"
-elif escolha == "número par":
+elif escolha == "/2":
     # Verifica se n1 E n2 são pares
     r = f"n1 par: {n1 % 2 == 0} | n2 par: {n2 % 2 == 0}"
-elif escolha == "número ímpar":
+elif escolha == "ímpar":
     r = f"n1 ímpar: {n1 % 2 != 0} | n2 ímpar: {n2 % 2 != 0}"
 
 print(f"Sua escolha foi: {escolha} e o resultado será: {r}")
