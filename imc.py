260c61e585ee6b9e63d1fffdcb0c101d3207567e
Mileni Lelis Ramos Servulo <lelis.engineer@yahoo.com>
2026-04-06 09:09:11 -0300

    Update imc.py

--- imc.py
+++ imc.py
@@ -11,30 +11,30 @@
 # 35 a < 40	'Obesidade Grau II (severa)'
 # ≥ 40		'Obesidade Grau III (mórbida)'
 
-
+import math
 #Entradas 
 p = float(input("Digite seu peso (kg): "))
 a = float(input("Digite sua altura (m): "))
-imc = p / (a ** 2)
-
+#imc = p / (a ** 2)
+imc = p / (pow(a, 2)
 #Processamentos
-if imc < 16:
-    classificacao = "Magreza grave"   #Váriaveil dentro de if,elif e else para que aja mais de um retorno dependendo do input do usuário
-elif 16 <= imc < 17:
-    classificacao = "Magreza moderada"
-elif 17 <= imc < 18.5:
-    classificacao = "Magreza leve" 
-elif 18.5 <= imc < 25:
-    classificacao = "Saudável"  
-elif 25 <= imc < 30:
-    classificacao = "Sobrepeso"   
-elif 30 <= imc < 35:
-    classificacao = "Obesidade Grau I" 
-elif 35 <= imc < 40:
-    classificacao = "Obesidade Grau II (severa)" 
-else:
-    classificacao = "Obesidade Grau III (mórbida)"  
-
+match imc:
+    case _ if imc < 16: 
+        classificacao = "Magreza grave"
+    case _ if 16 <= imc < 17: 
+        classificacao = "Magreza moderada"
+    case _ if 17 <= imc < 18.5: 
+        classificacao = "Magreza leve"
+    case _ if 18.5 <= imc < 25: 
+        classificacao = "Saudável"
+    case _ if 25 <= imc < 30: 
+        classificacao = "Sobrepeso"
+    case _ if 30 <= imc < 35: 
+        classificacao = "Obesidade Grau I"
+    case _ if 35 <= imc < 40: 
+        classificacao = "Obesidade Grau II (severa)"
+    case _: 
+        classificacao = "Obesidade Grau III (mórbida)"
 #Saída
 print(f"Seu IMC é: {imc:.2f} {classificacao}")
 
